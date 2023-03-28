b105ed85ab588d40f7427baff81e0ea98b58f38e
pheonixpark <90171172+pheonixpark@users.noreply.github.com>
2023-03-28 15:55:03 +0900

    기본세팅

---
+++ cookpro/src/main/java/com/myspring/cookpro/member/service/test01.java
@@ -0,0 +1,8 @@
+package com.myspring.cookpro.member.service;
+
+public class test01 {
+	public static void main(String[] args) {
+		System.out.println("Å×½ºÆ®02");
+	}
+
+}
--- cookpro/src/main/java/com/myspring/cookpro/member/test01.java
+++
@@ -1,8 +0,0 @@
-package com.myspring.cookpro.member;
-
-public class test01 {
-	public static void main(String[] args) {
-		System.out.println("Å×½ºÆ®01");
-	}
-
-}
